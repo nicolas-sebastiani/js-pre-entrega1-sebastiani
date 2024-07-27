9a5b60ada1395d00329fdc0b8c879d35f11b6896
nicolas <nicosebastiani90@gmail.com>
2024-07-26 21:40:42 -0300

    listo

--- js/1preentrega.js
+++ js/1preentrega.js
@@ -1,14 +1,25 @@
+//Es un calculador de precios de chapas metalicas de 1mt x 1.5mts, en el cual se pueden seleccionar 3 tipos de materiales (Aluminio, Acero al Carbono y Acero Inoxidable)
+// y con 5 espesores distintos que son los estandares en pulgadas pasados a milimetros. 
+
+// Para determinar el precio de determinada cantidad de chapas, primero consigo el peso usando el area y la densidad del material, luego lo multiplico por la 
+// cantidad y finalmente por el precio por kilo de chapa.
+
+
 alert("CHAPAS METALICAS SRL")
 alert("Calculadora de precio de chapas de metal de 1mt x 1.5mts")
 
 const precioDolar = 1480
+const precioKilo = 1800
 const densidadAluminio = 2.72
 const densidadAceroalCarbono = 7.84
 const densidadInoxidable = 8.03
+let peso = 0
 let material = 0
 let espesor = 0
 let cantidad = 0
 let precioFinal = 0
+
+// funcion que convierte el precio en pesos a dolares.
 
 function precioEnDolar (precioFinal) {
     let dolares = (precioFinal / precioDolar).toFixed(2)
@@ -22,29 +33,34 @@ while (material != 4){
         cantidad = parseInt(prompt('Ingrese cantidad de chapa a comprar: '));
         switch(espesor){
             case '1.6':
-                precioFinal = ((1.6 *( 150 * 100 )* densidadAluminio) * cantidad).toFixed(2);
+                peso = (1.6 *( 1.5 * 1 ) * densidadAluminio) * cantidad //calculo del peso de las chapas, utilizando el area, el espesor y la densidad.
+                precioFinal = (peso * precioKilo).toFixed(2); // calculo del precio, fijado a 2 lugares despues de la coma.
                 alert(`El precio de ${cantidad} chapas de Aluminio sera de $${precioFinal}`);
-                precioEnDolar(precioFinal.toFixed(2));
+                precioEnDolar(precioFinal);
                 break;
             case '2':
-                precioFinal = ((2 *( 150 * 100 )* densidadAluminio) * cantidad).toFixed(2);
+                peso = (2 *( 1.5 * 1 ) * densidadAluminio) * cantidad
+                precioFinal = (peso * precioKilo).toFixed(2);
                 alert(`El precio de ${cantidad} chapas de Aluminio sera de $${precioFinal}`);
-                precioEnDolar(precioFinal.toFixed(2));
+                precioEnDolar(precioFinal);
                 break;
             case '3.2':
-                precioFinal = ((3.2 *( 150 * 100 )* densidadAluminio) * cantidad).toFixed(2);
+                peso = (3.2 *( 1.5 * 1 ) * densidadAluminio) * cantidad
+                precioFinal = (peso * precioKilo).toFixed(2);
                 alert(`El precio de ${cantidad} chapas de Aluminio sera de $${precioFinal}`);
-                precioEnDolar(precioFinal.toFixed(2));
+                precioEnDolar(precioFinal);
                 break;
             case '4.7':
-                precioFinal = ((4.7 *( 150 * 100 )* densidadAluminio) * cantidad).toFixed(2);
+                peso = (4.7 *( 1.5 * 1 ) * densidadAluminio) * cantidad
+                precioFinal = (peso * precioKilo).toFixed(2);
                 alert(`El precio de ${cantidad} chapas de Aluminio sera de $${precioFinal}`);
-                precioEnDolar(precioFinal.toFixed(2));
+                precioEnDolar(precioFinal);
                 break;
             case '6.35':
-                precioFinal = ((6.35 *( 150 * 100 )* densidadAluminio) * cantidad).toFixed(2);
+                peso = (6.35 *( 1.5 * 1 ) * densidadAluminio) * cantidad
+                precioFinal = (peso * precioKilo).toFixed(2);
                 alert(`El precio de ${cantidad} chapas de Aluminio sera de $${precioFinal}`);
-                precioEnDolar(precioFinal.toFixed(2));
+                precioEnDolar(precioFinal);
                 break;
             }
     } else if (material == 2) {
@@ -52,29 +68,34 @@ while (material != 4){
         cantidad = parseInt(prompt('Ingrese cantidad de chapa a comprar: '));
         switch(espesor){
             case '1.6':
-                precioFinal = ((1.6 *( 150 * 100 )* densidadAceroalCarbono) * cantidad).toFixed(2);
+                peso = (1.6 *( 1.5 * 1 )* densidadAceroalCarbono) * cantidad
+                precioFinal = (peso * precioKilo).toFixed(2);
                 alert(`El precio de ${cantidad} chapas de Aluminio sera de $${precioFinal}`);
-                precioEnDolar(precioFinal.toFixed(2));
+                precioEnDolar(precioFinal);
                 break;
             case '2':
-                precioFinal = ((2 *( 150 * 100 )* densidadAceroalCarbono) * cantidad).toFixed(2);
+                peso = (2 *( 1.5 * 1 )* densidadAceroalCarbono) * cantidad
+                precioFinal = (peso * precioKilo).toFixed(2);
                 alert(`El precio de ${cantidad} chapas de Aluminio sera de $${precioFinal}`);
-                precioEnDolar(precioFinal.toFixed(2));
+                precioEnDolar(precioFinal);
                 break;
             case '3.2':
-                precioFinal = ((3.2 *( 150 * 100 )* densidadAceroalCarbono) * cantidad).toFixed(2);
+                peso = (3.2 *( 1.5 * 1 )* densidadAceroalCarbono) * cantidad
+                precioFinal = (peso * precioKilo).toFixed(2);
                 alert(`El precio de ${cantidad} chapas de Aluminio sera de $${precioFinal}`);
-                precioEnDolar(precioFinal.toFixed(2));
+                precioEnDolar(precioFinal);
                 break;
             case '4.7':
-                precioFinal = ((4.7 *( 150 * 100 )* densidadAceroalCarbono) * cantidad).toFixed(2);
+                peso = (4.7 *( 1.5 * 1 )* densidadAceroalCarbono) * cantidad
+                precioFinal = (peso * precioKilo).toFixed(2);
                 alert(`El precio de ${cantidad} chapas de Aluminio sera de $${precioFinal}`);
-                precioEnDolar(precioFinal.toFixed(2));
+                precioEnDolar(precioFinal);
                 break;
             case '6.35':
-                precioFinal = ((6.35 *( 150 * 100 )* densidadAceroalCarbono) * cantidad).toFixed(2);
+                peso = (6.35 *( 1.5 * 1 )* densidadAceroalCarbono) * cantidad
+                precioFinal = (peso * precioKilo).toFixed(2);
                 alert(`El precio de ${cantidad} chapas de Aluminio sera de $${precioFinal}`);
-                precioEnDolar(precioFinal.toFixed(2));
+                precioEnDolar(precioFinal);
                 break;
             }
     } else if (material == 3) {
@@ -82,27 +103,32 @@ while (material != 4){
         cantidad = parseInt(prompt('Ingrese cantidad de chapa a comprar: '));
         switch(espesor){
             case '1.6':
-                precioFinal = ((1.6 *( 150 * 100 )* densidadInoxidable) * cantidad).toFixed(2);
+                peso = (1.6 *( 1.5 * 1 )* densidadInoxidable) * cantidad
+                precioFinal = (peso * precioKilo).toFixed(2);
                 alert(`El precio de ${cantidad} chapas de Aluminio sera de $${precioFinal}`);
-                precioEnDolar(precioFinal.toFixed(2));
+                precioEnDolar(precioFinal);
                 break;
             case '2':
-                precioFinal = ((2 *( 150 * 100 )* densidadInoxidable) * cantidad).toFixed(2);
+                peso = (2 *( 1.5 * 1 )* densidadInoxidable) * cantidad
+                precioFinal = (peso * precioKilo).toFixed(2);
                 alert(`El precio de ${cantidad} chapas de Aluminio sera de $${precioFinal}`);
-                precioEnDolar(precioFinal.toFixed(2));
+                precioEnDolar(precioFinal);
                 break;
             case '3.2':
-                precioFinal = ((3.2 *( 150 * 100 )* densidadInoxidable) * cantidad).toFixed(2);
+                peso = (3.2 *( 1.5 * 1 )* densidadInoxidable) * cantidad
+                precioFinal = (peso * precioKilo).toFixed(2);
                 alert(`El precio de ${cantidad} chapas de Aluminio sera de $${precioFinal}`);
-                precioEnDolar(precioFinal.toFixed(2));
+                precioEnDolar(precioFinal);
                 break;
             case '4.7':
-                precioFinal = ((4.7 *( 150 * 100 )* densidadInoxidable) * cantidad).toFixed(2);
+                peso = (4.7 *( 1.5 * 1 )* densidadInoxidable) * cantidad
+                precioFinal = (peso * precioKilo).toFixed(2);
                 alert(`El precio de ${cantidad} chapas de Aluminio sera de $${precioFinal}`);
-                precioEnDolar(precioFinal.toFixed(2));
+                precioEnDolar(precioFinal);
                 break;
             case '6.35':
-                precioFinal = ((6.35 *( 150 * 100 )* densidadInoxidable) * cantidad).toFixed(2);
+                peso = (6.35 *( 1.5 * 1 )* densidadInoxidable) * cantidad
+                precioFinal = (peso * precioKilo).toFixed(2);
                 alert(`El precio de ${cantidad} chapas de Aluminio sera de $${precioFinal}`);
                 precioEnDolar(precioFinal);
                 break;
